58996f387b092aa31cfcc1dee009a800b3bfa821
protagonisths <13837050616@163.com>
2022-03-12 18:39:46 +0800

    feat(examples): add examples

---
+++ examples/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Examples from './components/Examples.vue'
+</script>
+<template>
+  <Examples/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
