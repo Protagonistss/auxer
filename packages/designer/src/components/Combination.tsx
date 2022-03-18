123ade5436e85a5268f0028c53f04f6edf2c3936
protagonisths <13837050616@163.com>
2022-03-18 23:50:01 +0800

    feat(designer): add draggable

--- packages/designer/src/components/Combination.tsx
+++ packages/designer/src/components/Combination.tsx
@@ -1,13 +1,40 @@
 /** @format */
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import vuedraggable from 'vuedraggable'
+import { NTag } from 'naive-ui'
 
 export default defineComponent({
   name: 'Combination',
+  components: { vuedraggable, NTag },
   setup() {
-    return {}
+    const pool = ref([])
+    const log = (e: Event) => {
+      console.log('e', e)
+    }
+    const slots = {
+      item: ({ element }: any) => (
+        <NTag type="success" style={{ width: '100%' }}>
+          {element.text}
+        </NTag>
+      )
+    }
+    return {
+      pool,
+      slots,
+      log
+    }
   },
   render() {
-    return <div>组合区</div>
+    return (
+      <vuedraggable
+        style={{ height: '200px', width: '100%' }}
+        list={this.pool}
+        group="people"
+        onChange={this.log}
+        v-slots={this.slots}
+        item-key="name"
+      ></vuedraggable>
+    )
   }
 })
--- packages/designer/src/components/Materials.tsx
+++ packages/designer/src/components/Materials.tsx
@@ -1,37 +1,56 @@
 /** @format */
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref, readonly } from 'vue'
 import { NLayout, NLayoutHeader, NLayoutContent, NTag } from 'naive-ui'
+import vuedraggable from 'vuedraggable'
 
 export default defineComponent({
   name: 'Materials',
-  components: { NTag },
+  components: { NTag, vuedraggable },
   setup() {
-    return {}
+    const materialPool = readonly(
+      ref([
+        { text: '输入框', id: 'input' },
+        { text: '下拉框', id: 'select' },
+        { text: '数字', id: 'inputNumber' },
+        { text: '日期', id: 'datepicker' }
+      ])
+    )
+
+    const vuedraggableSlots = {
+      item: ({ element }: { element: { text: string } }) => (
+        <section class={'dg-material__row'}>
+          <NTag type="success" style={{ width: '100%' }}>
+            {element.text}
+          </NTag>
+        </section>
+      )
+    }
+
+    const log = (e: Event) => {
+      console.log('e', e)
+    }
+    return {
+      materialPool,
+      vuedraggableSlots,
+      log
+    }
   },
   render() {
+    const { log } = this
     return (
       <NLayout>
         <NLayoutHeader bordered style={{ height: '32px', lineHeight: '32px' }}>
           物料区域
         </NLayoutHeader>
         <NLayoutContent style={{ height: 'calc(100vh - 32px)' }}>
-          <section class={'dg-material__row'}>
-            <NTag type="success" style={{ width: '100%' }}>
-              输入框
-            </NTag>
-            <NTag type="success" style={{ width: '100%' }}>
-              下拉框
-            </NTag>
-          </section>
-          <section class={'dg-material__row'}>
-            <NTag type="success" style={{ width: '100%' }}>
-              数字
-            </NTag>
-            <NTag type="success" style={{ width: '100%' }}>
-              日期
-            </NTag>
-          </section>
+          <vuedraggable
+            onChange={log}
+            item-key="name"
+            v-slots={this.vuedraggableSlots}
+            group={{ name: 'people', pull: 'clone', put: false }}
+            list={this.materialPool}
+          ></vuedraggable>
         </NLayoutContent>
       </NLayout>
     )
